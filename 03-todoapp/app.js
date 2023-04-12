402e211839fab02210b9aed886871bfa8c4735be
Tim Cai <tim.j.cai@gmail.com>
2023-04-12 18:18:38 +1000

    created to-doapp folder in node repo - created user stories for the app

---
+++ 03-todoapp/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const todoRoutes = require('./routes/todoRoutes')
+
+// express app
+const app = express()
+
+// mongoDB connection
+
+const dbURI =
+  'mongodb+srv://tester:adsfkj129hfkjasdf0123ug@cluster0.jqvaast.mongodb.net/to-do-app?retryWrites=true&w=majority'
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err))
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', todoRoutes)
+
+app.post('/', (req, res) => {
+  res.render
+})
