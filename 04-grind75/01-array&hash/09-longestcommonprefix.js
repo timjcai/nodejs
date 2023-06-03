78ceb7ebb86eecf884bd8e9f4121acd73cd947b5
Tim Cai <tim.j.cai@gmail.com>
2023-06-04 00:32:57 +1000

    finished longest common prefix- extremely hard implementing the index of method

--- 04-grind75/01-array&hash/09-longestcommonprefix.js
+++ 04-grind75/01-array&hash/09-longestcommonprefix.js
@@ -1,8 +1,21 @@
 var longestCommonPrefix = function(strs) {
-
+  let prefix = strs[0];
+  console.log(prefix)
+  for (let i = 0; i < strs.length; i++){
+    while (strs[i].indexOf(prefix) !== 0) {
+      console.log(strs[i])
+      prefix = prefix.slice(0, prefix.length - 1)
+      if (prefix === "") {
+        break
+      }
+    }
+  }
+  return prefix
 };
 
 const a = ["flower","flow","flight"]
 const b = ["dog","racecar","car"]
 
 // aim for linear time complexity - O(n)
+
+console.log(longestCommonPrefix(a))
